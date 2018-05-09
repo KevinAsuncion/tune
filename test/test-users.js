befcc68fb664229d8377b3d697512f9ff972c1e9
KevinAsuncion <kevinramosasuncion@gmail.com>
2018-05-09 10:11:42 -0700

    updated tests

--- test/test-users.js
+++ test/test-users.js
@@ -1,13 +1,22 @@
 'use strict';
 
 const chai = require('chai');
-const chatHttp = require('chai-http');
+const chaiHttp = require('chai-http');
 const { app, runServer, closeServer } = require('../server');
 
-
-const expect = chai.expect; 
+const expect = chai.expect;
 
 chai.use(chaiHttp);
+
+describe('user endpoint', function () {
+    it('should create a user', function () {
+        return chai.request(app)
+            .get('/')
+            .then(function (res) {
+                expect(res).to.have.status(200);
+            });
+    });
+});
 
 
 
